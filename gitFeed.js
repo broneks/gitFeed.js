c95edfa59bb1e1099e65e0a4ddcfb1619ba62619
Bszulc <bronekszulc@gmail.com>
2014-08-17 12:09:41 -0400

    refactoring

--- gitFeed.js
+++ gitFeed.js
@@ -8,45 +8,69 @@
 function gitFeed(settings) {
 	if (typeof settings === 'undefined' || typeof settings.username === 'undefined') {
 		console.log('gitFeed.js INIT ERROR: please initialize the plugin with a valid Github username (e.g gitFeed({ username: yourusername });)');
-		return false;
+		return;
 	}
 
-	var months     = (typeof settings.fullMonthNames !== 'undefined' && settings.fullMonthNames ? 
-		                ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'] : 
-		                ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']);
-	var $feed  	   = $('.gitfeed');
-	var numResults = (!isNaN(+settings.results) && isFinite(settings.results) ? settings.results : 5);
+	var months = typeof settings.fullMonthNames !== 'undefined' && settings.fullMonthNames ?
+                    ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'] : 
+                    ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+	
+	var $feed  = $('.gitfeed');
+	var numResults = !isNaN(+settings.results) && isFinite(settings.results) && settings.results > 0 ? settings.results : 5;
+
+	var populateFeed = function(data) {
+		var i, current, list, date, minutes, hours, day, month, year;
+		
+		for (i = 0; i < numResults; i++) {
+
+            current = data[i],
+            list    = [],
+            date    = new Date(current.created_at),
+            minutes = date.getMinutes(),
+            hours   = date.getHours(),
+            day     = date.getDate(),
+            month   = date.getMonth(),
+            year    = date.getFullYear();
+
+			if (current.payload.shas) {
+				list.push('<li><span class="gf-label">Commit:</span><a href="' + 
+                          current.url + 
+                          '" target="_blank">' + 
+                          current.payload.shas[0][2] + 
+                          '</a></li>');
+			}
+
+			if (current.repository) {
+				list.push('<li><span class="gf-label">Repo:</span><a href="' + 
+                          current.repository.url + 
+                          '" target="_blank">' + 
+                          current.repository.name + 
+                          '</a></li>');
+			}
+
+			list.push('<li><span class="gf-label">Action:</span><span>' + 
+                      (current.type).replace('Event', '') + 
+                      '</span></li>');
+
+			list.push('<li><span class="gf-label">Date:</span><span>' + 
+                      (day < 10 ? '0' : '') + day + 
+                      '-' + months[month] + '-' + 
+                      year + ' at ' + 
+                      (hours < 10 ? '0' : '') + hours + ':' + (minutes < 10 ? '0' : '') + minutes + 
+                      '</span></li>');
+				
+			$feed.append('<ul>' + (list.toString()).replace(/>,</g, '><') + '</ul>');
+		}
+	};
 
 	$.ajax({
 		url: 'https://github.com/' + settings.username + '.json', 
 		dataType: 'jsonp',
 		success: function(data) {
-			for (var i = 0; i < numResults; i++) {
-				var that    = data[i],
-					list    = [],
-					date    = new Date(that.created_at),
-					minutes = date.getMinutes(),
-					hours   = date.getHours(),
-					day     = date.getDate(),
-					month   = date.getMonth(),
-					year    = date.getFullYear();
-
-				if (that.payload.shas) {
-					list.push('<li><span class="gf-label">Commit:</span><a href="' + that.url + '" target="_blank">' + that.payload.shas[0][2] + '</a></li>');
-				}
-
-				if (that.repository) {
-					list.push('<li><span class="gf-label">Repo:</span><a href="' + that.repository.url + '" target="_blank">' + that.repository.name + '</a></li>');
-				}
-
-				list.push('<li><span class="gf-label">Action:</span><span>' + (that.type).replace('Event', '') + '</span></li>');
-				list.push('<li><span class="gf-label">Date:</span><span>' + (day < 10 ? '0' : '') + day + "-" + months[month] + "-" + year + " at " + (hours < 10 ? '0' : '') + hours + ":" + (minutes < 10 ? '0' : '') + minutes + '</span></li>');
-					
-				$feed.append('<ul>' + (list.toString()).replace(/>,</g, '><') + '</ul>');
-			}
+			populateFeed(data);
 		},
-		error: function(err) {
-			console.log('gitFeed.js AJAX ERROR: ' + JSON.stringify(err, null, 2));
+		error: function(error) {
+			console.log('gitFeed.js AJAX ERROR: ' + JSON.stringify(error, null, 2));
 		}
 	});
 }
